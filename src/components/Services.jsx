78ea93cc267a506df34abb3628309e9f173b1892
Hamjaster <hamzasepal@gmail.com>
2023-11-28 17:42:44 +0500

    resposive

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -16,10 +16,12 @@ export default function Services() {
 
     return (
         <div className='my-44'>
-            <h1 className={`' text-6xl my-28 text-center font-semibold ${dancing.className} '`}>Our Services</h1>
-            <div className='flex flex-row justify-between items-center w-full'>
 
-                <ul className='[&>*]:py-4  font-light relative'>
+            <h1 className={`' text-6xl my-28 text-center font-semibold ${dancing.className} '`}>Our Services</h1>
+
+            <div className='px-6 flex flex-col md:flex-row justify-between items-center w-full'>
+
+                <ul className='[&>*]:py-4 flex flex-row flex-wrap items-center justify-center md:block font-light relative'>
                     <li onClick={() => setService('destination')} className={` ${service === "destination" ? " bg-gray-100 text-black " : " text-gray-400 "} pl-7 pr-28 uppercase cursor-pointer  tracking-widest font-medium`}>Destinations</li>
 
                     <li onClick={() => setService('corporate')} className={` ${service === "corporate" ? " bg-gray-100 text-black " : " text-gray-400 "} pl-7 pr-28 uppercase cursor-pointer  tracking-widest font-medium`}>Corporate</li>
@@ -31,8 +33,8 @@ export default function Services() {
 
                     <li onClick={() => setService('social')} className={` ${service === "social" ? " bg-gray-100 text-black " : " text-gray-400 "} pl-7 pr-28 uppercase cursor-pointer  tracking-widest font-medium`}>Social</li>
 
-
-                    <div className="w-[36rem] pl-28 pr-12 flex flex-col justify-center space-y-5  h-[150%] mx-8 bg-white absolute top-1/2 left-full -translate-y-1/2">
+                    {/* white box in PC */}
+                    <div className="hidden md:flex w-[25rem] lg:w-[36rem] pl-7 lg:pl-28 pr-7 lg:pr-12 flex-col justify-center space-y-5 h-[110%] lg:h-[150%] mx-8 bg-white absolute top-1/2 left-full -translate-y-1/2">
                         <div className="font-mono uppercase tracking-[0.1rem]">{data[service][0]}</div>
                         <div className="text-3xl font-normal">
                             {data[service][1]}
@@ -41,8 +43,21 @@ export default function Services() {
                     </div>
 
                 </ul>
-                <img className='w-[47rem]' src={data[service][3]} alt="" srcset="" />
 
+                <div className="flex md:hidden items-start w-full py-8 flex-col justify-center space-y-3  h-[150%] mx-8 bg-white">
+
+                    <div className="font-mono uppercase tracking-[0.1rem]">{data[service][0]}</div>
+                    <div className="text-2xl font-normal">
+                        {data[service][1]}
+                    </div>
+                    <div className="text-base underline cursor-pointer">{data[service][2]}</div>
+
+                </div>
+
+                {/* <img className='w-[47rem]' src={data[service][3]} alt="" srcset="" /> */}
+                <div style={{ backgroundImage: `url(${data[service][3]})` }} className="image bg-center bg-cover w-full h-[40rem] md:w-[40rem]">
+
+                </div>
             </div>
         </div>
     )
