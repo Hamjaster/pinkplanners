b580b0a9b81fc755c98444609392f01afc1c228e
Hamjaster <hamzasepal@gmail.com>
2023-12-03 17:33:24 +0500

    just

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ const dancing = Dancing_Script({ subsets: ['latin'], weight: ['400', '500', '600
 
 export default function About() {
     return (
-        <section className="bg-gray-100 flex space-y-6 flex-col py-24 sm:py-44 px-10 sm:px-36 text-center items-center justify-center">
+        <section className="bg-gray-100 flex space-y-6 flex-col py-20 sm:py-36 px-10 sm:px-36 text-center items-center justify-center">
 
-            <p className=' text-xl sm:text-2xl font-thin'>DIAMOL - Combining inventive concepts with an  organized event planning process</p>
+            <p className=' text-xl sm:text-3xl font-thin'>DIAMOL - Combining inventive concepts with an  organized event planning process</p>
 
-            <strong className='text-lg sm:text-xl'>We are an inventive event agency.</strong>
+            <strong className='text-lg sm:text-2xl'>We are an creative event agency.</strong>
 
         </section>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaInstagram } from "react-icons/fa6";
+import { FaInstagram, FaPinterest } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
@@ -9,16 +9,15 @@ export default function Footer() {
         <div className='bg-black flex flex-row-reverse justify-between items-center px-12 py-8 text-base text-white'>
 
             <div className="logos gap-4 text-2xl sm:text-3xl items-center grid grid-cols-2 sm:grid-cols-4">
-                <div className='hover:text-gray-300 cursor-pointer'><FaInstagram /></div>
-                <div className='hover:text-gray-300 cursor-pointer'><FaFacebook /></div>
-                <div className='hover:text-gray-300 cursor-pointer'><FaTwitter /></div>
                 <div className='hover:text-gray-300 cursor-pointer'><FaWhatsapp /></div>
+                <div className='hover:text-gray-300 cursor-pointer'><FaInstagram /></div>
+                <div className='hover:text-gray-300 cursor-pointer'><FaPinterest /></div>
+                <div className='hover:text-gray-300 cursor-pointer'><FaFacebook /></div>
             </div>
 
             <div className='text-gray-300 text-sm sm:text-base font-extralight flex flex-col sm:flex-row sm:space-x-3 space-y-1 sm:space-y-0 '>
                 <span className=''>© 2023 Diamol Events. All rights reserved.</span>
-                <span className=' underline cursor-pointer'>Privacy Policy</span>
-                <span className=' underline cursor-pointer'>Terms of Agreement</span>
+
             </div>
 
         </div>
